62d703389632c0a4c5fbf416280198995efd4aa7
Mike Kinghan <imkinghan@gmail.com>
2014-08-05 18:08:20 +0100

    Documentation of `lexicon`

--- src/lexicon.h
+++ src/lexicon.h
@@ -38,39 +38,39 @@
  **************************************************************************/
 #include "platform.h"
 
-/*! \file lexicon.h
-    This file defines symbolic constants for some preprocessor keywords.
-*/
+/** \file lexicon.h
+ *   This file defines symbolic constants for some preprocessor keywords.
+ */
 
-//! Constant denoting the if directive
+/// Constant denoting the if directive
 #define TOK_IF          "if"
-//! Constant denoting the `#ifdef` directive
+/// Constant denoting the `#ifdef` directive
 #define TOK_IFDEF       "ifdef"
-//! Constant denoting the `#ifndef` directive
+/// Constant denoting the `#ifndef` directive
 #define TOK_IFNDEF      "ifndef"
-//! Constant denoting the `#else` directive
+/// Constant denoting the `#else` directive
 #define TOK_ELSE        "else"
-//! Constant denoting the `#elif` directive
+/// Constant denoting the `#elif` directive
 #define TOK_ELIF        "elif"
-//! Constant denoting the `#endif` directive
+/// Constant denoting the `#endif` directive
 #define TOK_ENDIF       "endif"
-//! Constant denoting the `#define` directive
+/// Constant denoting the `#define` directive
 #define TOK_DEFINE      "define"
-//! Constant denoting the `#undef` directive
+/// Constant denoting the `#undef` directive
 #define TOK_UNDEF       "undef"
-//! Constant denoting the `#include` directive
+/// Constant denoting the `#include` directive
 #define TOK_INCLUDE     "include"
-//! Constant denoting the `#pragma` directive
+/// Constant denoting the `#pragma` directive
 #define TOK_PRAGMA      "pragma"
-//! Constant denoting the `line` directive
+/// Constant denoting the `line` directive
 #define TOK_LINE     "line"
-//! Constant denoting the `#error` directive
+/// Constant denoting the `#error` directive
 #define TOK_ERROR       "error"
-//! Constant denoting the `defined` operator
+/// Constant denoting the `defined` operator
 #define TOK_DEFINED     "defined"
-//! Constant denoting the alternative boolean negation operator
+/// Constant denoting the alternative boolean negation operator
 #define TOK_ALT_BOOLEAN_NOT "not"
-//! Constant denoting the alternative bitwise negation operator
+/// Constant denoting the alternative bitwise negation operator
 #define TOK_ALT_BIT_NOT 	"compl"
 
-#endif /* EOF*/
+#endif // EOF
